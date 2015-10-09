d797670a5be62fd0ba493d557a29c56275523013
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-09 10:50:59 -0400

    Moved Gigawatts folder to TIY but unable to have the updated work showing on Xcode

---
+++ Gigawatts/TimeCircuitsViewController.swift
@@ -0,0 +1,10 @@
+//
+//  TimeCircuitsViewController.swift
+//  Gigawatts
+//
+//  Created by Elizabeth Yeh on 10/8/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
