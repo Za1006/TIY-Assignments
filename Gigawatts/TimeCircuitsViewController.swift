ca8478c5ba5a7a8454a6357445d4a2fd3085c0eb
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-09 21:57:12 -0400

    added TimeCircuitsViewController Xcode

--- Gigawatts/TimeCircuitsViewController.swift
+++ Gigawatts/TimeCircuitsViewController.swift
@@ -8,3 +8,15 @@
 
 import UIKit
 
+import UIKit
+
+@objc protocol DatePickerDelegate
+{
+    func dateWasChosen(dateCount: Int)
+}
+
+class TimeCircutsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
+    @IBOutlet var picker: UIPickerView!
+    @IBOutlet weak var timeLabel: UILabel!
+    var date: NSDateFormatter?
