cab95318e0eafd0eead70a2caef8e34b49b514cd
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-12 14:34:49 -0400

    added HeroDetailViewController file

---
+++ HeroTracker/HeroTracker/HeroDetailViewController .swift
@@ -0,0 +1,15 @@
+//
+//  HeroDetailViewController .swift
+//  HeroTracker
+//
+//  Created by Elizabeth Yeh on 10/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class HeroDetailViewController: UIViewController
+{
+    
+}
+
