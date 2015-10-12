ea14b0dbe5907f68ab2419396294b7478793d05c
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-12 14:29:37 -0400

    added second view controller, named identifier,

---
+++ HeroTracker/HeroTracker/Hero.swift
@@ -0,0 +1,14 @@
+//
+//  Hero.swift
+//  HeroTracker
+//
+//  Created by Elizabeth Yeh on 10/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+class Hero
+{
+    
+}
