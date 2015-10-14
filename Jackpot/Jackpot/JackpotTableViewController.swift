4cb246933942503f00c674c8da65a7ab3475c76e
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-13 22:56:05 -0400

    edit tableview

--- Jackpot/Jackpot/JackpotTableViewController.swift
+++ Jackpot/Jackpot/JackpotTableViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class JackpotTableViewController: UITableViewController
 
 {
-
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -22,19 +23,22 @@ class JackpotTableViewController: UITableViewController
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
