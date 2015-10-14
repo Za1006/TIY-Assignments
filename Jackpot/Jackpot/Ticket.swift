e32d2619a39e5b5ff809900ff974eef27e4196fb
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-14 07:53:38 -0400

    Updated storyboard.
    delete view controller, added Ticket.swift file

---
+++ Jackpot/Jackpot/Ticket.swift
@@ -0,0 +1,15 @@
+//
+//  ticket.swift
+//  Jackpot
+//
+//  Created by Elizabeth Yeh on 10/14/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+class Ticket
+{
+    
+}
+    
