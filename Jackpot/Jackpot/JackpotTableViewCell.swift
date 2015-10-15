b8329c919e17a10a720a35f1a2665c9d96d75c5f
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-14 20:12:54 -0400

    created tableviewcell.swift

--- Jackpot/Jackpot/JackpotTableViewCell.swift
+++ Jackpot/Jackpot/JackpotTableViewCell.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 
-class JackpotTableViewCell: UITableViewCell {
+class JackpotTableViewCell: UITableViewCell
+{
 
-    override func awakeFromNib() {
+    override func awakeFromNib()
+    {
         super.awakeFromNib()
         // Initialization code
     }
 
     @IBOutlet weak var numbersLabel: UILabel!
-    override func setSelected(selected: Bool, animated: Bool) {
+    override func setSelected(selected: Bool, animated: Bool)
+    {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
