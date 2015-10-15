1315c7526a07b85a63d439feff9b5d7e08c1617a
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-14 22:16:27 -0400

    Created picker view and started class

---
+++ Jackpot/Jackpot/winnerPickerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  winnerPickerViewController.swift
+//  Jackpot
+//
+//  Created by Elizabeth Yeh on 10/14/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class winnerPickerViewController: UIPickerView,UIPickerViewDelegate
+{
+    
+}
