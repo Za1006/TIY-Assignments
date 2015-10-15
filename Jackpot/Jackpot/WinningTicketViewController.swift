dc4144c5190f749f9ad6fff50f03e6c15e537d41
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-15 16:36:12 -0400

    added code from homework review

--- Jackpot/Jackpot/WinningTicketViewController.swift
+++ Jackpot/Jackpot/WinningTicketViewController.swift
@@ -12,37 +12,60 @@ class WinningTicketViewController: UIViewController, UIPickerViewDataSource, UIP
 {
 
     @IBOutlet weak var picker: UIPickerView!
-    
-    
-    
-    @IBAction func checkTickets(sender: UIButton)
-    {
-        
-    }
-    
-    
-    @IBAction func cancel(sender: UIButton)
-    {
-        
-    }
-    
+    var delegate: WinningTicketViewControllerDelegate?
+    var winningPicks = [Int](count: 6, repeatedValue: 0)
     
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int
+    {
+        return 6
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return 53
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return "\(row + 1)"
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        winningPicks[component] = row+1
+    }
+    
+    
+    @IBAction func checkTickets(sender: UIButton)
+    {
+        if winningPicks.count == 6
+        {
+            let winningTicket = JackpotTicket(picksArray: winningPicks)
+            delegate?.winningTicketWasAdded(winningTicket)
+        }
+    }
+    
+    
+    @IBAction func cancel(sender: UIButton)
+    {
+       self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+       /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
