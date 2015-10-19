1a0764fd4ef594b1350140c95b87ec7bd505d09f
Elizabeth Elliott <elliott1006@gmail.com>
2015-10-19 13:38:28 -0400

    Created cell.swift file and tableViewController in storyboard

---
+++ The Grail Diary/The Grail Diary/SiteNameCell.swift
@@ -0,0 +1,27 @@
+//
+//  SiteNameCell.swift
+//  The Grail Diary
+//
+//  Created by Elizabeth Yeh on 10/19/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class SiteNameCell: UITableViewCell
+{
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
