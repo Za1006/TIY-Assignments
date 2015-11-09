9a35617217a121ee128b34fdcaf1964892e7a89c
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-09 12:23:31 -0500

    initial commit for OutaTime Redux

---
+++ OutaTime Redux/OutaTime Redux/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutaTime Redux
+//
+//  Created by Elizabeth Yeh on 11/9/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
