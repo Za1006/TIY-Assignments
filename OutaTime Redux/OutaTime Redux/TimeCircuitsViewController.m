f41463aea267b125c7a104f6cf8ffc324d191434
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-09 14:16:05 -0500

    Connected labels and outlets for veiwControllers

--- OutaTime Redux/OutaTime Redux/TimeCircuitsViewController.m
+++ OutaTime Redux/OutaTime Redux/TimeCircuitsViewController.m
@@ -10,16 +10,19 @@
 
 @interface TimeCircuitsViewController ()
 
+
 @end
 
 @implementation TimeCircuitsViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
