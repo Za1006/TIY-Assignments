dbbcb05affbc7cf29b42f7c7e410ff05a60d8085
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-10 12:50:08 -0500

    checked the ViewControllers and table view

---
+++ JackpotObjectiveC/JackpotObjectiveC/Jackpot.m
@@ -0,0 +1,9 @@
+//
+//  Jackpot.m
+//  JackpotObjectiveC
+//
+//  Created by Elizabeth Yeh on 11/10/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ JackpotObjectiveC/JackpotObjectiveC/JackpotCell.m
@@ -0,0 +1,9 @@
+//
+//  JackpotCell.m
+//  JackpotObjectiveC
+//
+//  Created by Elizabeth Yeh on 11/10/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
