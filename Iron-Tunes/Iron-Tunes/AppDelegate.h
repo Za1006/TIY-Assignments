c8f726597da258380c033d7d925877d145dc1d7a
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-11 09:21:32 -0500

    wrong file

---
+++ Iron-Tunes/Iron-Tunes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Iron-Tunes
+//
+//  Created by Elizabeth Yeh on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Iron-Tunes/Iron-Tunes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Iron-Tunes
+//
+//  Created by Elizabeth Yeh on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
