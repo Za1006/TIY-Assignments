4f81f0760d50d22d424fca7865d60b63c955cd9d
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-12 13:30:54 -0500

    Initial commit for Movie Mania

---
+++ MovieMania/MovieMania/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieMania
+//
+//  Created by Elizabeth Yeh on 11/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieMania/MovieMania/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieMania
+//
+//  Created by Elizabeth Yeh on 11/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
