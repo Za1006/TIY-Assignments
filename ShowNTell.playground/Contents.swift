31fb61628344ea86408551d31b16c39337895375
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-30 20:02:02 -0500

    making examples of Optional Chaining

--- ShowNTell.playground/Contents.swift
+++ ShowNTell.playground/Contents.swift
@@ -1,3 +1,26 @@
 //: Playground - noun: a place where people can play
+//Optional Chaining - Alternative to Forced Unwrapping
+class Person
+{
+    var guest: Guest?
+}
+
+class Guest
+{
+    var numberOfPlates = 1
+}
+
+let tom = Person()
+let tableCount = tom.guest?.numberOfPlates
+tom.guest = Guest()
 
 
+if let tableCount = tom.guest?.numberOfPlates
+{
+    print("Tom's guest has \(tableCount) plate. ")
+}
+else
+{
+    print("No guests")
+}
+// to retrive number of plates if guests exsists,the answer will show as nil value. But when assign "Guest" instance to tom, value is no longer nil.
