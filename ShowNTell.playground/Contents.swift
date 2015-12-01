5c491b55eac4b4e3a6e337b25bf2d1e7947bbb06
Elizabeth Elliott <elliott1006@gmail.com>
2015-11-30 22:50:06 -0500

    adding comments

--- ShowNTell.playground/Contents.swift
+++ ShowNTell.playground/Contents.swift
@@ -1,9 +1,14 @@
+import UIKit
+
 //: Playground - noun: a place where people can play
 //Optional Chaining - Alternative to Forced Unwrapping
+// If the optional is nil, the property, method or subscript call returns nil.
 class Person
 {
     var guest: Guest?
 }
+// specify optional by placing question mark (?) after the optional value you wish to call a property, method or subscript. 
+// (!) is to force the unwrapping of its value.
 
 class Guest
 {
@@ -24,3 +29,8 @@ else
     print("No guests")
 }
 // to retrive number of plates if guests exsists,the answer will show as nil value. But when assign "Guest" instance to tom, value is no longer nil.
+
+
+
+
+
