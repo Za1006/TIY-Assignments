891f7982aba23e0024faae0c59ddf79e212923ed
Elizabeth Elliott <elliott1006@gmail.com>
2015-12-05 20:14:00 -0500

    initial commit (take 25!)

--- CarFinder/CarFinder/AppDelegate.swift
+++ CarFinder/CarFinder/AppDelegate.swift
@@ -15,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        Parse.setApplicationId("ePoyWqeccFKgWrYz4UZit7ZSWQ8aruGEdYUO09LE",
+            clientKey: "REK38ZOXFpIhpuaxpKGI5d4KUfR8a53PUlEaZcJY")
         return true
     }
 
@@ -25,17 +27,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func applicationDidEnterBackground(application: UIApplication)
+    {
+        let navController = window?.rootViewController as! UINavigationController
+        let loginVC = navController.viewControllers[0] as! LoginViewController
+        loginVC.saveCityData()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    func applicationDidBecomeActive(application: UIApplication)
+    {
+        
+        let navController = window?.rootViewController as! UINavigationController
+        let loginVC = navController.viewControllers[0] as! LoginViewController
+        loginVC.loadCityData()
     }
 
     func applicationWillTerminate(application: UIApplication) {
